3f5a7fb0bb5a06a3954f2dcfcb7c9bce3c08306a
bdke <91001643+bdke@users.noreply.github.com>
2023-09-09 21:06:24 +0800

    Added Base completion

--- server/jmcserver/Handlers/JMCCompletionHandler.cs
+++ server/jmcserver/Handlers/JMCCompletionHandler.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JMCLSP.Datas;
+using JMCLSP.Helper;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
@@ -12,6 +15,12 @@ namespace JMCLSP.Handlers
 {
     internal class JMCCompletionHandler : CompletionHandlerBase
     {
+        private readonly ILogger<JMCCompletionHandler> _logger;
+
+        public JMCCompletionHandler(ILogger<JMCCompletionHandler> logger)
+        {
+            _logger = logger;
+        }
         public override async Task<CompletionItem> Handle(CompletionItem request, CancellationToken cancellationToken) => request;
         public override async Task<CompletionList> Handle(CompletionParams request, CancellationToken cancellationToken)
         {
@@ -27,17 +36,47 @@ namespace JMCLSP.Handlers
             var funcs = ExtensionData.Workspaces.GetJMCFunctionDefines()
                 .SelectMany(v => v.Tokens)
                 .DistinctBy(v => v.Value);
+
+            //function completion
+            if (request.Context?.TriggerCharacter == ".")
+            {
+                var requestToken = lexer.GetJMCToken(request.Position);
+                if (requestToken != null)
+                {
+                    var index = lexer.Tokens.IndexOf(requestToken);
+                    var token = lexer.Tokens[index];
+                    if (token.TokenType == Lexer.JMC.Types.JMCTokenType.LITERAL)
+                    {
+                        var splited = token.Value
+                            .Split(' ')
+                            .Last()
+                            .Split('.')
+                            .Where(v => !string.IsNullOrWhiteSpace(v));
+                        var result = FunctionHierarchy.GetHierachy(funcs.Select(v => v.Value), splited);
+                        return CompletionList.From(result.Select(value => new CompletionItem()
+                        {
+                            Label = value.FuncName,
+                            Kind = ( value.Type == FunctionHierarchyType.CLASS ) ? CompletionItemKind.Class : CompletionItemKind.Function,
+                            InsertText = value.Type == FunctionHierarchyType.CLASS ? value.FuncName : $"{value.FuncName}()",
+                        }));
+                    }
+                }
+            }
             var vars = ExtensionData.Workspaces.GetJMCVariables()
-                .SelectMany(v => v.Tokens)
+            .SelectMany(v => v.Tokens)
                 .Where(v => !v.Value.EndsWith(".get", StringComparison.CurrentCulture))
                 .DistinctBy(v => v.Value);
 
-            foreach ( var func in funcs )
+            //normal completion
+            var funcValue = FunctionHierarchy.GetFirstHierarchy(funcs.Select(v => v.Value).ToArray());
+            _logger.LogDebug(LoggerHelper.ObjectToJson(funcValue));
+            foreach (var value in funcValue)
             {
                 list.Add(new()
                 {
-                    Label = func.Value,
-                    Kind = CompletionItemKind.Function,
+                    Label = value.FuncName,
+                    Kind = (value.Type == FunctionHierarchyType.CLASS) ? CompletionItemKind.Class : CompletionItemKind.Function,
+                    InsertText = value.Type == FunctionHierarchyType.CLASS ? value.FuncName : $"{value.FuncName}()",
                 });
             }
 
@@ -53,6 +92,8 @@ namespace JMCLSP.Handlers
 
             return new(list);
         }
+
+
         protected override CompletionRegistrationOptions CreateRegistrationOptions(CompletionCapability capability, ClientCapabilities clientCapabilities) => new() 
         {
             ResolveProvider = true,
